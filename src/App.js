4306ceee648ac812d23b1115fd27d63c8145a906
Portafolio FullStack Developer de ArturoTexada <arturo.tejada@gmail.com>
2022-12-14 11:25:58 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+//import ExpenseItem from "./components/ExpenseItem";
+import React from "react";
+import DisplayExpenses from "./components/Expenses/DisplayExpenses";
+
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Sheet Trim",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Angle L2x2",
+      amount: 799.49,
+      date: new Date(2021, 2, 12),
+    },
+    {
+      id: "e3",
+      title: "Plate 3/16",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "Purlin 24C1246",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  return (
+      <div>
+        <h2>Let's get started!</h2>
+          <DisplayExpenses items={expenses} />
+      </div>
+  );
+
+  // return React.createElement(
+  //     'div', {},
+  //       React.createElement('h2', {}, "Let's get started!"),
+  //       React.createElement(DisplayExpenses, {items: expenses})
+  // );
+
+
+
+
+
+
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom/client';
+
+import './index.css';
+import App1 from './App';
+import App2 from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App1 />);
+root.render(<App2 />);
